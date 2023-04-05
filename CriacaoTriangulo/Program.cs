2bbe2a5a647f8b1837884059d89928addcc20a75
bernardoalvesmelo <bernardoalvesdemelo123@gmail.com>
2023-04-05 17:43:32 -0300

    Add project files.

---
+++ CriacaoTriangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CriacaoTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] lados = new double[3];
+            for (int i = 0; i < lados.Length; i++)
+            {
+                Console.Write("Digite o {0} lado: ", i + 1);
+                lados[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Triangulo triangulo = new Triangulo(lados);
+            Console.WriteLine(triangulo.VerificarTipo());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CriacaoTriangulo/Triangulo.cs
@@ -0,0 +1,46 @@
+﻿namespace CriacaoTriangulo
+{
+    public class Triangulo
+    {
+        double[] lados;
+        public Triangulo(double[] lados)
+        {
+            this.lados = lados;
+        }
+
+        public string VerificarTipo()
+        {
+            if (!VerificarLados())
+            {
+                return "Não forma triângulo";
+            }
+
+            if (lados[0] == lados[1])
+            {
+                if (lados[1] == lados[2])
+                {
+                    return "Forma triângulo equilátero";
+                }
+
+                return "Forma triângulo isósceles";
+            }
+
+            if (lados[0] == lados[2])
+            {
+                return "Forma triângulo isósceles";
+            }
+
+            if (lados[1] == lados[2])
+            {
+                return "Forma triângulo isósceles";
+            }
+
+            return "Forma triângulo escaleno";
+        }
+
+        private bool VerificarLados()
+        {
+            return lados[0] < lados[1] + lados[2] && lados[1] < lados[0] + lados[2] && lados[2] < lados[0] + lados[1];
+        }
+    }
+}
